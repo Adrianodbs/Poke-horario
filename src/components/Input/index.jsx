11f44d58a25a46d4fe54a275e715eb0c9a14875d
Adriano <adriano_alves_2008@hotmail.com>
2023-07-27 15:21:04 -0300

    Feat update date and save local storage

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,15 +1,50 @@
+import { useState, useEffect } from 'react'
+import { format, addDays, parseISO } from 'date-fns'
 import './input.css'
 
-export default function Input({ title, respawn }) {
+export default function Input({ title }) {
+  const [selectedDate, setSelectedDate] = useState('')
+  const [nextRespawnDate, setNextRespawnDate] = useState('')
+
+  useEffect(() => {
+    // Recuperar a data salva no Local Storage ao montar o componente
+    const storedDate = localStorage.getItem(title)
+    if (storedDate) {
+      setSelectedDate(storedDate)
+    }
+  }, [title])
+
+  useEffect(() => {
+    if (selectedDate) {
+      // Armazenar a nova data no Local Storage sempre que houver uma atualização
+      localStorage.setItem(title, selectedDate)
+
+      const initialDate = parseISO(selectedDate)
+      const respawnInterval = title.includes('Boss') ? 12 : 3
+      const nextDate = addDays(initialDate, respawnInterval)
+      setNextRespawnDate(format(nextDate, 'yyyy-MM-dd'))
+    } else {
+      setNextRespawnDate('')
+    }
+  }, [selectedDate, title])
+
   return (
     <div className="data">
       <label className="label">
         <span>{title}</span>
-        <input type="date" />
+        <input
+          type="date"
+          value={selectedDate}
+          onChange={e => setSelectedDate(e.target.value)}
+        />
       </label>
-      <h4>
-        Respawn: <p>Dia {respawn}</p>
-      </h4>
+      {nextRespawnDate ? (
+        <h4>
+          Respawn: <p>Dia {nextRespawnDate}</p>
+        </h4>
+      ) : (
+        <h4>Nenhuma data cadastrada</h4>
+      )}
     </div>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,9 +4,15 @@ import './home.css'
 export default function Home() {
   return (
     <div className="container">
-      <h1>Poke Horário</h1>
-      <form>
+      <form className="form">
         <Input title="Boss de Kanto" />
+        <Input title="Boss de Johto" />
+        <Input title="Boss de Hoenn" />
+        <Input title="Boss de Sinnoh" />
+      </form>
+      <form className="form">
+        <Input title="Dig de Kanto" />
+        <Input title="Dig de Johto" />
       </form>
     </div>
   )
