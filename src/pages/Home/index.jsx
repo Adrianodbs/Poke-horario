bc4347fdb9aa75b89e605cc72ebaa96f23bc2185
Adriano <adriano_alves_2008@hotmail.com>
2023-07-28 15:38:39 -0300

    Style: css input and home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,16 +4,23 @@ import './home.css'
 export default function Home() {
   return (
     <div className="container">
-      <form className="form">
-        <Input title="Boss de Kanto" />
-        <Input title="Boss de Johto" />
-        <Input title="Boss de Hoenn" />
-        <Input title="Boss de Sinnoh" />
-      </form>
-      <form className="form">
-        <Input title="Dig de Kanto" />
-        <Input title="Dig de Johto" />
-      </form>
+      <div className="content">
+        <h3>Tempo de respawn do boss (12dias)</h3>
+        <form className="form">
+          <Input title="Boss de Kanto" />
+          <Input title="Boss de Johto" />
+          <Input title="Boss de Hoenn" />
+          <Input title="Boss de Sinnoh" />
+        </form>
+      </div>
+
+      <div className="content">
+        <h3>Tempo de reaspawn do dig (3dias)</h3>
+        <form className="form">
+          <Input title="Dig de Kanto" />
+          <Input title="Dig de Johto" />
+        </form>
+      </div>
     </div>
   )
 }
