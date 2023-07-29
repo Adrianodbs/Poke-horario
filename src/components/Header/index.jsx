6c1796fd25b061efd7250703e3d647ba0e219061
Adriano <adriano_alves_2008@hotmail.com>
2023-07-29 14:36:33 -0300

    Style: active buttons header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,35 @@
 import './header.css'
 import pokebola from '../../assets/img/pokebola.png'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function Header() {
+  const [activeLink, setActiveLink] = useState('home')
+
+  const handleLinkClick = link => {
+    setActiveLink(link)
+  }
   return (
     <header>
       <img src={pokebola} alt="pokebola" />
-      <ul>
+      <ul className="listMenu">
         <li>
-          <Link to="/">Home</Link>
+          <Link
+            to="/"
+            className={activeLink === 'home' ? 'active' : ''}
+            onClick={() => handleLinkClick('home')}
+          >
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/dicas">Dicas</Link>
+          <Link
+            to="/dicas"
+            className={activeLink === 'dicas' ? 'active' : ''}
+            onClick={() => handleLinkClick('dicas')}
+          >
+            Dicas
+          </Link>
         </li>
       </ul>
       <h4>Bem vindo treinador</h4>
